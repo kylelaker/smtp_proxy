b53260d39eb11cabb7a454d4e9d43e2ae66eea15
Kyle Laker <kyle@laker.email>
2020-05-10 22:01:40 -0400

    Create config directory

--- install.sh
+++ install.sh
@@ -6,6 +6,7 @@ dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" > /dev/null 2>&1 && pwd)"
 
 sudo echo "" > /dev/null
 sudo apt install $(awk '{printf "python3-%s ", $0}' requirements.txt)
+sudo mkdir -p /etc/smtp_proxy/
 sudo mkdir -p /etc/systemd/system
 sudo cp "$dir/smtp-proxy.service" /etc/systemd/system/
 sudo mkdir -p /usr/local/bin/
