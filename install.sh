2871ad6c738f5e9355a0aabfc700abc4904143b2
Kyle Laker <kyle@laker.email>
2020-05-10 21:54:14 -0400

    Install dependencies

--- install.sh
+++ install.sh
@@ -5,6 +5,7 @@ set -e
 dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" > /dev/null 2>&1 && pwd)"
 
 sudo echo "" > /dev/null
+sudo apt install $(awk '{printf "python3-%s ", $0}' requirements.txt)
 sudo mkdir -p /etc/systemd/system
 sudo cp "$dir/smtp-proxy.service" /etc/systemd/system/
 sudo mkdir -p /usr/local/bin/
